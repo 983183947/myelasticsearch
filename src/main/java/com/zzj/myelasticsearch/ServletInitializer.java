5423a9e961be2cf8cf758590d512a7c14e886cdb
zzj <983183947@qq.com>
2017-09-20 16:24:19 +0800

    数据库可连接

--- src/main/java/com/zzj/myelasticsearch/ServletInitializer.java
+++ src/main/java/com/zzj/myelasticsearch/ServletInitializer.java
@@ -1,9 +1,10 @@
 package com.zzj.myelasticsearch;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+
+public class ServletInitializer  extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
